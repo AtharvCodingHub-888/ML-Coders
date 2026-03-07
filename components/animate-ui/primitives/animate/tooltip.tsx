3a33507eb0da1c4fac24f37edc97af9c8f4437aa
AtharvCodingHub-888 <shingeatharva888@gmail.com>
2026-03-07 06:36:50 +0530

    Add files via upload
    Frontend

---
+++ components/animate-ui/primitives/animate/tooltip.tsx
@@ -0,0 +1,10 @@
+// This file has been emptied/stubbed to prevent import errors with missing @floating-ui/react package.
+export const Tooltip = ({ children, ...props }: any) => <>{children}</>;
+export const TooltipTrigger = ({ children, ...props }: any) => <>{children}</>;
+export const TooltipContent = ({ children, ...props }: any) => <>{children}</>;
+export const TooltipProvider = ({ children, ...props }: any) => <>{children}</>;
+
+export interface TooltipProps { }
+export interface TooltipTriggerProps { }
+export interface TooltipContentProps { }
+export interface TooltipProviderProps { }
